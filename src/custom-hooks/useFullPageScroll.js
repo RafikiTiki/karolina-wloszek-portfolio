1bba4f46b19a0b104a94fd9d11e24341d67f7094
RafikiTiki <wow1926@gmail.com>
2019-07-22 22:10:53 +0200

    Scrolling on index page & get rid of hamsterjs

--- src/custom-hooks/useFullPageScroll.js
+++ src/custom-hooks/useFullPageScroll.js
@@ -1,47 +1,82 @@
-import { useEffect, useState } from 'react'
-import Hamster from "hamsterjs"
-import { window } from 'browser-monads'
-import smoothscroll from 'smoothscroll-polyfill'
+import { useEffect, useState } from "react"
+import { document, window } from "browser-monads"
+import smoothscroll from "smoothscroll-polyfill"
 
 export function useFullPageScroll() {
   const [isOnBottomPage, onSetIsOnBottomPage] = useState(false)
   smoothscroll.polyfill()
 
   useEffect(() => {
-    // if (typeof document !== `undefined` && typeof window !== 'undefined') {
-      const scrollToSecondPage = () => {
-        const pageHeight = window.innerHeight
-        window.scrollTo({
-          top: pageHeight,
-          left: 0,
-          behavior: 'smooth',
-        })
-      }
+    let yDown = null
 
-      const scrollToFirstPage = () => {
-        window.scrollTo({
-          top: 0,
-          left: 0,
-          behavior: 'smooth',
-        })
-      }
+    const scrollToSecondPage = () => {
+      window.scrollTo({
+        top: window.innerHeight,
+        left: 0,
+        behavior: "smooth",
+      })
+    }
 
-      const hamster = Hamster(document.getElementById('index-page'))
+    const scrollToFirstPage = () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      })
+    }
 
-      hamster.wheel((event, delta, deltaX, deltaY) => {
-        event.preventDefault()
-        if (isOnBottomPage) {
+    const handleScroll = event => {
+      if (isOnBottomPage) {
+        if (event.deltaY < 0) {
+          event.preventDefault()
           onSetIsOnBottomPage(false)
           scrollToFirstPage()
-        } else {
+        }
+      } else if (event.deltaY > 0) {
+        event.preventDefault()
+        scrollToSecondPage()
+        onSetIsOnBottomPage(true)
+      }
+    }
+
+    const handleTouchStart = event => {
+      yDown = event.touches[0].clientY
+    }
+
+    const handleTouchMove = event => {
+      if (!yDown) {
+        return
+      }
+
+      const yUp = event.touches[0].clientY
+      const yDiff = yDown - yUp
+
+      if (yDiff > 0) {
+        if (!isOnBottomPage) {
+          event.preventDefault()
           scrollToSecondPage()
           onSetIsOnBottomPage(true)
         }
-      })
-
-      return () => {
-        hamster.unwheel()
+      } else {
+        if (isOnBottomPage) {
+          event.preventDefault()
+          onSetIsOnBottomPage(false)
+          scrollToFirstPage()
+        }
       }
+
+      yDown = null
+    }
+
+    document.addEventListener("wheel", handleScroll)
+    document.addEventListener("touchstart", handleTouchStart)
+    document.addEventListener("touchmove", handleTouchMove, { passive: false })
+
+    return () => {
+      document.removeEventListener("wheel", handleScroll)
+      document.removeEventListener("touchstart", handleTouchStart)
+      document.removeEventListener("touchmove", handleTouchMove, { passive: false })
+    }
   }, [isOnBottomPage])
 
   return !isOnBottomPage
